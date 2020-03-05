940bd153b07a3b0b8534dcec3a9cc122b50c9d6d
phucledien <phucledien@gmail.com>
2020-03-06 03:27:04 +0700

    [Feature] Remove unnecessary settings
    - Remove unnecessary settings: Hide dock icon and keep app last state
    - Refactor code
    - Remove background color of preferences window
    - Update new app pictures

--- hidden/Common/Util.swift
+++ hidden/Common/Util.swift
@@ -13,26 +13,7 @@ import ServiceManagement
 
 class Util {
     
-    static var numberOfSecondForAutoHide: Double {
-        get {
-            let numberOfSeconds = UserDefaults.standard.double(forKey: Notification.Name.numberOfSecondForAutoHide)
-            
-            //For very first time, set it equal 10
-            if numberOfSeconds == 0.0 {
-                let defaultValue = 10.0
-                UserDefaults.standard.set(defaultValue, forKey: Notification.Name.numberOfSecondForAutoHide)
-                return defaultValue
-            }
-            
-            return numberOfSeconds
-        }
-        set {
-            UserDefaults.standard.set(newValue, forKey: Notification.Name.numberOfSecondForAutoHide)
-        }
-    }
-    
-    static func setUpAutoStart(isAutoStart:Bool)
-    {
+    static func setUpAutoStart(isAutoStart:Bool) {
         let launcherAppId = "com.dwarvesv.LauncherApplication"
         let runningApps = NSWorkspace.shared.runningApplications
         let isRunning = !runningApps.filter { $0.bundleIdentifier == launcherAppId }.isEmpty
@@ -45,131 +26,9 @@ class Util {
         }
     }
     
-    static func setIsAutoStart(_ isAutoStart:Bool){
-        UserDefaults.standard.set(isAutoStart, forKey: IS_AUTO_START)
-        
-    }
-    
-    static func getIsCollapse() -> Bool{
-        let savedValue = UserDefaults.standard.bool(forKey: IS_COLLAPSE)
-        return savedValue
-    }
-    
-    static func setIsCollapse(_ isCollapse:Bool){
-        UserDefaults.standard.set(isCollapse, forKey: IS_COLLAPSE)
-        
-    }
-    
-    static func getIsAutoStart() -> Bool{
-        let savedValue = UserDefaults.standard.bool(forKey: IS_AUTO_START)
-        return savedValue
-    }
-    
-    
-    
-    static func getStateAutoStart() -> NSControl.StateValue{
-        if(getIsAutoStart())
-        {
-            return .on
-        }
-        return .off
-    }
-    
-    static func setIsAutoHide(_ isAutoHide:Bool){
-        UserDefaults.standard.set(isAutoHide, forKey: IS_AUTO_HIDE)
-        
-    }
-    
-    static func getIsAutoHide()->Bool{
-        let isAutoHide = UserDefaults.standard.bool(forKey: IS_AUTO_HIDE)
-        return isAutoHide
-    }
-    
-    static func getStateAutoHide() -> NSControl.StateValue{
-        if(getIsAutoHide())
-        {
-            return .on
-        }
-        return .off
-    }
-    
-    static func setIsKeepInDock(_ isKeepInDock:Bool){
-        UserDefaults.standard.set(isKeepInDock, forKey: IS_KEEP_IN_DOCK)
-        
-    }
-    
-    static func getIsKeepInDock() -> Bool{
-        UserDefaults.standard.register(defaults: [IS_KEEP_IN_DOCK : true])
-        let savedValue = UserDefaults.standard.bool(forKey: IS_KEEP_IN_DOCK)
-        return savedValue
-    }
-    
-    static func getStateKeepInDock() -> NSControl.StateValue {
-        if(getIsKeepInDock())
-        {
-            return .on
-        }
-        return .off
-    }
-    
-    static func getShowPreferences() -> Bool {
-        UserDefaults.standard.register(defaults: [IS_SHOW_PREFERENCES : true])
-        return UserDefaults.standard.bool(forKey: IS_SHOW_PREFERENCES)
-    }
-    
-    static func setShowPreferences(_ isShowPreferences: Bool) {
-        UserDefaults.standard.set(isShowPreferences, forKey: IS_SHOW_PREFERENCES)
-    }
-    
-    static func getStateShowPreferences() -> NSControl.StateValue {
-        return getShowPreferences() ? .on : .off
-    }
-    
-    
-    static func getKeepLastState() -> Bool {
-        let savedValue = UserDefaults.standard.bool(forKey: IS_KEEP_LAST_STATE)
-        return savedValue
-    }
-    
-    static func setKeepLastState(_ isKeepLastState: Bool) {
-        UserDefaults.standard.set(isKeepLastState, forKey: IS_KEEP_LAST_STATE)
-    }
-    
-    static func getStateKeepLastState() -> NSControl.StateValue {
-        return getKeepLastState() ? .on : .off
-    }
-    
-    
-    static func isMenuOpened() -> Bool {
-        let options = CGWindowListOption(arrayLiteral: .excludeDesktopElements, .optionOnScreenOnly)
-        let windowsListInfo = CGWindowListCopyWindowInfo(options, CGWindowID(0))
-        let infoList = windowsListInfo as! [[String:Any]]
-        let names = infoList.map { dict in
-            return dict["kCGWindowOwnerName"] as? String
-            }.filter({ (name) -> Bool in
-                name == App_Name
-            })
-        return names.count >= 2
-    }
-    
     static func showPrefWindow() {
-        let prefWindow = PreferencesWindowController.shared.window!
-        Util.bringToFront(window: prefWindow)
+        let prefWindow = PreferencesWindowController.shared.window
+        prefWindow?.bringToFront()
     }
-    
-    static func toggleDockIcon(_ state: Bool) -> Bool {
-        var result: Bool
-        if state {
-            result = NSApp.setActivationPolicy(NSApplication.ActivationPolicy.regular)
-        }
-        else {
-            result = NSApp.setActivationPolicy(NSApplication.ActivationPolicy.accessory)
-        }
-        return result
-    }
-    
-    static func bringToFront(window:NSWindow?) {
-        window?.makeKeyAndOrderFront(nil)
-        NSApp.activate(ignoringOtherApps: true)
-    }
+   
 }
