adce9c261f12208d523b9c9c204d228aeb54a1a9
Peter Luo <zl2005.luo@icloud.com>
2021-05-31 19:04:42 +0800

    Support multiple screens

--- hidden/Features/StatusBar/MouseOverDetector.swift
+++ hidden/Features/StatusBar/MouseOverDetector.swift
@@ -18,24 +18,17 @@ class MouseOverDetector {
     var entersAreaHandler : ()->Void
     var leavesAreaHandler : (Bool)->Void // $0: whether or not the mouse has clicked inside the area before leaving
     
-    private var statusBarY  : CGFloat
+    private var statusBarY  : CGFloat = .nan
     private var mouseMoveMoniter : EventMoniter?
     private var mouseClickMoniters  = [EventMoniter]()
+    
+    private var previousMouseLocation: CGPoint = .zero
     
     init(entersArea entersAreaHandler: @escaping ()->Void, leavesArea leavesAreaHandler: @escaping (Bool)->Void) {
         self.entersAreaHandler = entersAreaHandler
         self.leavesAreaHandler = leavesAreaHandler
         
-        if let statusbarRect = WindowInfo.cgRectOfWindow(named: "Menubar"),
-           let currentScreen = NSScreen.main {
-            // Cocoa uses top to down y coords but CoreGraphics uses down to top coords
-            // To avoid convertion everytime, statusBarY is inverted on initialize
-            statusBarY = currentScreen.frame.height - statusbarRect.height
-            print(statusbarRect.height)
-        } else {
-            os_log("Unable to get status bar height")
-            statusBarY = .nan
-        }
+        updateMenubarMinY()
         mouseMoveMoniter = EventMoniter(eventType: .mouseMoved, handler: mouseMoved)
         mouseClickMoniters = [
             EventMoniter(eventType: .leftMouseUp , handler: mouseClicked),
@@ -53,7 +46,24 @@ class MouseOverDetector {
         }
     }
     
+    private func updateMenubarMinY() {
+        if let statusbarRect = WindowInfo.cgRectOfWindow(named: "Menubar"),
+           let currentScreen = Util.screenWithMouse {
+            // Cocoa uses top to down y coords but CoreGraphics uses down to top coords
+            // To avoid convertion everytime, statusBarY is inverted on initialize
+            statusBarY = currentScreen.frame.height - statusbarRect.height
+        } else {
+            os_log("unable to get menu bar height")
+        }
+    }
+    
     private func mouseMoved(_ event: NSEvent) {
+        // must have at least 1 pixel of difference before proceeding
+        guard !(NSEvent.mouseLocation ~= previousMouseLocation) else { return }
+        if NSScreen.screens.count > 1 {
+            updateMenubarMinY()
+        }
+        
         if mouseIsInArea {
             if NSEvent.mouseLocation.y < statusBarY { // leaves area
                 leavesAreaHandler(mouseClickedInsideArea)
