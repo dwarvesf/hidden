e923c7a248086f324d66453847d4135406d54085
UeharaYou <jack1617728914+github@gmail.com>
2023-07-10 15:29:13 +0900

    Commit: Hotfix
    + Fixed Issues of not properly handling status bar item position detection on startup, when multiple display are in use.

--- HiddenBar/Features/StatusBar/StatusBarController.swift
+++ HiddenBar/Features/StatusBar/StatusBarController.swift
@@ -35,8 +35,8 @@ class StatusBarController {
             let secondarySepratorX = instance.secondarySeprator.button?.getOrigin?.x
         else {return .invalid}
         
-        // all x will be 0 if applicationDidFinishLaunching have not returned, so we have to try again
-        if toggleButtonX == 0 && primarySepratorX == 0 && secondarySepratorX == 0 {return .onStartUp}
+        // all x will be all equal if applicationDidFinishLaunching have not returned, so we have to try again
+        if toggleButtonX == primarySepratorX && primarySepratorX == secondarySepratorX {return .onStartUp}
         
         if Global.isUsingLTRTypeSystem {
             return (toggleButtonX > primarySepratorX && primarySepratorX > secondarySepratorX) ? .valid : .invalid
